7cc08256df08851a10ba6c0a004bf4cb37f68d65
buivantoana <toanbui219@gmail.com>
2024-01-12 23:29:04 +0700

    update

--- functions/api.js
+++ functions/api.js
@@ -1,50 +1,19 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
+import router from "./routes";
+import cors from "cors";
+
 const app = express();
-const router = express.Router();
 
-let records = [];
+app.use(cors());
+app.use(express.json());
 
-//Get all students
-router.get('/', (req, res) => {
-  res.send('App is running..');
-});
+mongoose
+  .connect(
+    `mongodb+srv://root:123@cluster0.pnvccqv.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Database Connected!"));
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
+app.use("/api", router);
 
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
-
-app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
