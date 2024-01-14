29e641cf0989691f0ffed455ae93b299f2d2ba12
buivantoana <toanbui219@gmail.com>
2024-01-14 15:22:06 +0700

    connect

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,14 @@ const express = require("express");
 const serverless = require("serverless-http");
 const router = require("./routes/index");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+mongoose
+  .connect(
+    `mongodb+srv://root:123@cluster0.pnvccqv.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Database Connected!"));
+
 app.use(cors());
 app.use("/api", router);
 module.exports.handler = serverless(app);
